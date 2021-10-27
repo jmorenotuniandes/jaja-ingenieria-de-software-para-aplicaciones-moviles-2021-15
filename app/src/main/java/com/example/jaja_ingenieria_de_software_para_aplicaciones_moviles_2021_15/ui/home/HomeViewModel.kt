14fd35dadfeae03982d45568a8ab7adc4b49ef1b
Jhon Jairo <jhonmore@bancolombia.com>
2021-10-27 00:03:05 -0500

    Se agrega primer commit de la aplicación

---
+++ app/src/main/java/com/example/jaja_ingenieria_de_software_para_aplicaciones_moviles_2021_15/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.jaja_ingenieria_de_software_para_aplicaciones_moviles_2021_15.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/test/java/com/example/jaja_ingenieria_de_software_para_aplicaciones_moviles_2021_15/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.jaja_ingenieria_de_software_para_aplicaciones_moviles_2021_15
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
