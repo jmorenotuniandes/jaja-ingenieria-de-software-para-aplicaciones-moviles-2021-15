4482f9e6c2cef9b2fc8a5d289c9270aff469c1d4
Andrés Cendales <andres01660@gmail.com>
2021-11-06 18:14:49 -0500

    VIN-59 Include artist viewmodel and model

--- app/src/main/java/com/example/vinyl/ui/artists/ArtistsFragment.kt
+++ app/src/main/java/com/example/vinyl/ui/artists/ArtistsFragment.kt
@@ -4,42 +4,70 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.vinyl.adapters.AlbumAdapter
+import com.example.vinyl.adapters.ArtistAdapter
 import com.example.vinyl.databinding.FragmentArtistsBinding
+import com.example.vinyl.model.dto.Artist
 import com.example.vinyl.viewmodel.ArtistsViewModel
 
 class ArtistsFragment : Fragment() {
 
-    private lateinit var artistsViewModel: ArtistsViewModel
     private var _binding: FragmentArtistsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var artistsViewModel: ArtistsViewModel
+    private var viewModelAdapter: ArtistAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-     /*   artistsViewModel =
-            ViewModelProvider(this).get(ArtistsViewModel::class.java)*/
-
         _binding = FragmentArtistsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        /*val textView: TextView = binding.textArtists
-        artistsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })*/
+        viewModelAdapter = ArtistAdapter()
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        recyclerView = binding.artistsRv
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = viewModelAdapter
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val activity = requireNotNull(this.activity){
+            "You can only access the viewModel after onActivityCreated()"
+        }
+        artistsViewModel = ViewModelProvider(this, ArtistsViewModel.Factory(activity.application))
+            .get(ArtistsViewModel::class.java)
+        artistsViewModel.artists.observe(viewLifecycleOwner, Observer<List<Artist>>{
+            it.apply{
+                viewModelAdapter!!.artist = this
+            }
+        })
+        artistsViewModel.eventNetworkError.observe(viewLifecycleOwner, Observer<Boolean>{
+            isNetworkError -> if (isNetworkError) onNetworkError()
+        })
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun onNetworkError(){
+        if(!artistsViewModel.isNetworkErrorShown.value!!){
+            Toast.makeText(activity, "Network Error", Toast.LENGTH_LONG).show()
+            artistsViewModel.onNetworkErrorShown()
+        }
     }
 }
--- app/src/main/java/com/example/vinyl/viewmodel/ArtistsViewModel.kt
+++ app/src/main/java/com/example/vinyl/viewmodel/ArtistsViewModel.kt
@@ -1,15 +1,53 @@
 package com.example.vinyl.viewmodel
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.vinyl.model.dto.Artist
+import com.example.vinyl.repository.ArtistRepository
 
 class ArtistsViewModel (application: Application) : AndroidViewModel(application) {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Artists Fragment"
+    private val _artists = MutableLiveData<List<Artist>>()
+    val artists: LiveData<List<Artist>>
+        get() = _artists
+
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+
+    private var artistRepository = ArtistRepository(application)
+
+    init {
+        refreshDataFromNetwork()
     }
-    val text: LiveData<String> = _text
+
+    private fun refreshDataFromNetwork(){
+        artistRepository.refreshData({
+                _artists.postValue(it)
+                _eventNetworkError.value = false
+                _isNetworkErrorShown.value = false
+            },
+            {
+                _eventNetworkError.value = true
+            }
+        )
+    }
+
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+    class Factory(val app: Application) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(ArtistsViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return ArtistsViewModel(app) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
 }
