1a12176e76ec3ed28d016eade2eeea6de40f6592
Juan Vanegas <j.vanegaso@uniandes.edu.co>
2021-11-09 19:24:53 -0500

    VIN-8: Add Details View for Collectors

--- app/src/main/java/com/example/vinyl/model/dto/Collector.kt
+++ app/src/main/java/com/example/vinyl/model/dto/Collector.kt
@@ -1,9 +1,13 @@
 package com.example.vinyl.model.dto
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Collector (
     val collectorId: Int,
     val name:String,
     val telephone:String,
     val email:String,
     val bgColor: String? = null
-)
+) : Parcelable
---
+++ app/src/main/java/com/example/vinyl/ui/collectors/CollectorDetailsFragment.kt
@@ -0,0 +1,46 @@
+package com.example.vinyl.ui.collectors
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.navArgs
+import com.example.vinyl.databinding.FragmentCollectorDetailsBinding
+
+
+class CollectorDetailsFragment : Fragment() {
+
+    private var binding: FragmentCollectorDetailsBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCollectorDetailsBinding.inflate(inflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val args: CollectorDetailsFragmentArgs by navArgs()
+
+        args.let {
+            val collector = it.collectorDetails
+            (requireActivity() as AppCompatActivity).supportActionBar?.title = collector.name
+            binding!!.txtCollectorName.text = collector.name
+            // TODO: Setup the rest of the elements of this view
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
+}
