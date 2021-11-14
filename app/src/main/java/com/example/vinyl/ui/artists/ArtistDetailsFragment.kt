17d3b92be85f397ed2526207acf7069b75f01167
Andrés Cendales <andres01660@gmail.com>
2021-11-14 09:51:38 -0500

    VIN-06 Remove unused method

--- app/src/main/java/com/example/vinyl/ui/artists/ArtistDetailsFragment.kt
+++ app/src/main/java/com/example/vinyl/ui/artists/ArtistDetailsFragment.kt
@@ -13,10 +13,6 @@ class ArtistDetailsFragment : Fragment() {
 
     private var binding: FragmentArtistsDetailsBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
